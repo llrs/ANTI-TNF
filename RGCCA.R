b9b41c2aeb3fed2593709bfa445a1ac5336bf611
llrs <lluis.revilla@gmail.com>
2018-01-12 10:57:00 +0100

    Correct several errors
    Using calculated shrinkage parameter.
    Correct labeling.

--- RGCCA.R
+++ RGCCA.R
@@ -3,6 +3,7 @@ load("filtered_data.RData")
 source("helper_functions.R")
 
 library("RGCCA")
+library("ggforce")
 
 # Prepare meta for RGCCA
 variables <- c("Patient_ID", "Time", "IBD", "Aftected_area", "Treatment")
@@ -21,10 +22,10 @@ C <- matrix(0, ncol = length(A), nrow = length(A),
 C <- subSymm(C, "16S", "metadata", 1)
 C <- subSymm(C, "RNAseq", "metadata", 1)
 
-
 # We cannnot comput eht tau.estimate for A[[1]]
 # (shrinkage <- sapply(A, tau.estimate))
-shrinkage <- c(0.5, 0, 1) # We guess a 0.5 for the RNAseq expression
+shrinkage <- c(0.1034826, 0, 1) # We calculated the shrinkage for the RNAseq
+# expression in the server
 shrinkage[2] <- tau.estimate(A[[2]])
 (min_shrinkage <- sapply(A, function(x){1/sqrt(ncol(x))}))
 # # Don't let the shrinkage go below the thershold allowed
@@ -68,7 +69,6 @@ samples <- data.frame("RNAseq" = sgcca.centroid$Y[["RNAseq"]][, 1],
                       "microbiota" = sgcca.centroid$Y[["16S"]][, 1])
 
 # Colors for the plots
-names(colors) <- unique(meta_R$ID)
 
 samples <- cbind(samples, meta)
 samples$Patient_ID <- as.factor(samples$Patient_ID)
@@ -80,35 +80,33 @@ pdf(paste0("Figures/", today, "_RGCCA_plots.pdf"))
 samples$Time <- factor(samples$Time, levels(as.factor(samples$Time))[c(4, 1, 2, 3)])
 for (p in seq_along(levels(samples$Time))){
     a <- ggplot(samples, aes(RNAseq, microbiota)) +
-        geom_text(aes(color =  ID, label = ID)) +
+        geom_text(aes(color =  Patient_ID, label = Patient_ID)) +
         geom_vline(xintercept = 0) +
         geom_hline(yintercept = 0) +
         ggtitle(paste0("Samples by time")) +
         xlab("RNAseq (component 1)") +
         ylab("16S (component 1)") +
-        guides(col = guide_legend(title="Patient")) +
+        guides(col = FALSE) +
         theme(plot.title = element_text(hjust = 0.5)) +
-        scale_color_manual(values = colors) +
         facet_wrap_paginate(~Time, ncol = 1, nrow = 1, page = p)
     print(a)
 }
 
 for (p in seq_along(levels(samples$ID))){
     a <- ggplot(samples, aes(RNAseq, microbiota)) +
-        geom_text(aes(color =  ID, label = ifelse(!is.na(labels),
-                                                  paste(Time, labels, sep = "_"),
-                                                  as.character(Time)))) +
+        geom_text(aes(color =  Patient_ID, label = Patient_ID)) +
         geom_vline(xintercept = 0) +
         geom_hline(yintercept = 0) +
         ggtitle(paste0("Samples by patient")) +
         xlab("RNAseq (component 1)") +
         ylab("16S (component 1)") +
-        guides(col = guide_legend(title="Patient")) +
+        guides(col = FALSE) +
         theme(plot.title = element_text(hjust = 0.5)) +
         scale_color_manual(values = colors) +
         facet_wrap_paginate(~ID, ncol = 1, nrow = 1, page = p)
     print(a)
 }
+
 ggplot(samples, aes(RNAseq, microbiota)) +
     geom_text(aes(color =  Patient_ID,
                   label = Sample_Code)) +
@@ -117,7 +115,18 @@ ggplot(samples, aes(RNAseq, microbiota)) +
     ggtitle("All samples at all times ") +
     # xlab("RNAseq (component 1)") +
     # ylab("16S (component 1)") +
-    guides(col = guide_legend(title="Patient")) +
+    guides(col = FALSE) +
+    theme(plot.title = element_text(hjust = 0.5))
+
+ggplot(samples, aes(RNAseq, microbiota)) +
+    geom_text(aes(color = Time,
+                  label = Patient_ID)) +
+    geom_vline(xintercept = 0) +
+    geom_hline(yintercept = 0) +
+    ggtitle("All samples at all times ") +
+    xlab("RNAseq (component 1)") +
+    ylab("16S (component 1)") +
+    guides(col = guide_legend(title = "Time")) +
     theme(plot.title = element_text(hjust = 0.5))
 
 
@@ -129,7 +138,7 @@ ggplot(samples, aes(RNAseq, microbiota)) +
     ggtitle("All samples at all times ") +
     xlab("RNAseq (component 1)") +
     ylab("16S (component 1)") +
-    guides(col = guide_legend(title="Responders")) +
+    guides(col = guide_legend(title = "Responders")) +
     theme(plot.title = element_text(hjust = 0.5))
 
 ggplot(samples, aes(RNAseq, microbiota)) +
@@ -186,18 +195,20 @@ ggplot(subVariables, aes(comp1, comp2), color = Origin) +
 
 pr <- prcomp(t(expr[subVariables$var[subVariables$Origin == "RNAseq"],]), scale. = TRUE)
 prS <- summary(pr)
-ggplot(as.data.frame(pr$x), aes(PC1, PC2, color = as.factor(meta_R$ANTITNF_responder))) +
+ggplot(as.data.frame(pr$x), aes(PC1, PC2, color = as.factor(meta$ANTITNF_responder))) +
     geom_point() +
-    xlab(paste("PC1", prS$importance[2, "PC1"]*100)) +
-    ylab(paste("PC2", prS$importance[2, "PC2"]*100)) +
+    xlab(paste("PC1", prS$importance[2, "PC1"]*100, "%")) +
+    ylab(paste("PC2", prS$importance[2, "PC2"]*100, "%")) +
+    guides(col = guide_legend("Anti TNF responder")) +
     ggtitle("RNAseq PCA from the important variables")
 
 pr <- prcomp(t(otus[subVariables$var[subVariables$Origin == "16S"],]), scale. = TRUE)
 prS <- summary(pr)
-ggplot(as.data.frame(pr$x), aes(PC1, PC2, color = as.factor(meta_R$HSCT_responder))) +
+ggplot(as.data.frame(pr$x), aes(PC1, PC2, color = as.factor(meta$ANTITNF_responder))) +
     geom_point() +
-    xlab(paste("PC1", prS$importance[2, "PC1"]*100)) +
-    ylab(paste("PC2", prS$importance[2, "PC2"]*100)) +
+    xlab(paste("PC1", prS$importance[2, "PC1"]*100, "%")) +
+    ylab(paste("PC2", prS$importance[2, "PC2"]*100, "%")) +
+    guides(col = guide_legend("Anti TNF responder")) +
     ggtitle("16S PCA from the important variables")
 
 # Plot for the same component the variables of each block
