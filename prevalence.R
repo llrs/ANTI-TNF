95fba33999ed561f1f5bfd014882888f69e63122
llrs <lluis.revilla@gmail.com>
2018-01-25 11:18:32 +0100

    Start prevalence analysis

---
+++ prevalence.R
@@ -0,0 +1,100 @@
+# Summarize by taxa
+library("metagenomeSeq")
+
+load("filtered_data.RData")
+source("helper_functions.R")
+source("helper_prevalence.R")
+
+# Match the name of meta and rownames
+otus_table_i <- otus_table_i[, match(rownames(meta_i), colnames(otus_table_i))]
+
+MR_i <- newMRexperiment(
+  otus_table_i,
+  phenoData = AnnotatedDataFrame(meta_i),
+  featureData = AnnotatedDataFrame(as.data.frame(otus_tax_i))
+)
+
+
+genus_i <- aggTax(MR_i, lvl = "Genus", out = "matrix")
+species_i <- aggTax(MR_i, lvl = "Species", out = "matrix")
+family_i <- aggTax(MR_i, lvl = "Family", out = "matrix")
+order_i <- aggTax(MR_i, lvl = "Order", out = "matrix")
+class_i <- aggTax(MR_i, lvl = "Class", out = "matrix")
+phylum_i <- aggTax(MR_i, lvl = "Phylum", out = "matrix")
+
+# Compare at the otus level the time and response effect
+otus <- response_time(otus_table_i, meta_i)
+write.csv(otus, "prevalence_otus.csv")
+genus_i <- response_time(genus_i, meta_i)
+write.csv(genus_i, "prevalence_genus_i.csv")
+species_i <- response_time(species_i, meta_i)
+write.csv(species_i, "prevalence_species.csv")
+family_i <- response_time(family_i, meta_i)
+write.csv(family_i, "prevalence_family.csv")
+order_i <- response_time(order_i, meta_i)
+write.csv(order_i, "prevalence_order.csv")
+class_i <- response_time(class_i, meta_i)
+write.csv(class_i, "prevalence_class.csv")
+phylum_i <- response_time(phylum_i, meta_i)
+write.csv(phylum_i, "prevalence_phylum.csv")
+
+# summary:  Not significative, IBD patients are as  likely to have one
+# microorganism as the controls.
+
+# Test the prevalence between non controls at times T0, T26, T52 ####
+removeControls <- meta_i$IBD == "CD"
+removeTimes <- meta_i$Time %in% c("T0", "T26", "T52")
+keep <- removeControls & removeTimes
+res <- prevalence_tab(
+  otus_table_i[, keep], meta_i[keep, ],
+  c("Time", "HSCT_responder")
+)
+
+# Fisher test
+Time_Responder <- prevalence(res$presence, res$absence)
+
+Time_Responder[is.na(Time_Responder)] <- 1
+Time_Responder <- p.adjust(Time_Responder, "BH")
+
+if (any(Time_Responder < 0.05)) {
+  message("Plots!!")
+}
+
+# The presence of microorganisms thorough time is not different in the
+# overall patients
+
+# Test if the responders and the non responders behave differently along time
+# If the presence of microorganisms thorough time is different in responders
+# than in non-responders ####
+otus <- comp(otus_table_i, meta_i)
+write.csv(otus, "otus_time.csv")
+species <- comp(species_i, meta_i)
+write.csv(species, "species_time.csv")
+genus <- comp(genus_i, meta_i)
+write.csv(genus, "genus_time.csv")
+family <- comp(family_i, meta_i)
+write.csv(family, "family_time.csv")
+order <- comp(order_i, meta_i)
+write.csv(order, "order_time.csv")
+class <- comp(class_i, meta_i)
+write.csv(class, "class_time.csv")
+phylum <- comp(phylum_i, meta_i)
+write.csv(phylum, "phylum_time.csv")
+
+# Bootstrapp while controlling by Time ####
+
+# # bootstrapping with 1000 replications
+# results <- boot(data = t(sweep(wocontrols, 2, colSums(wocontrols), `/`) > 0.005),
+#                 statistic = ratio,
+#                 R = 1000,
+#                 meta = meta_i[removeControls, ], columns = "Time",
+#                 parallel = "multicore", ncpus = 4)
+#
+# # view results
+# results
+# plot(results)
+#
+# # get 95% confidence interval
+# boot.ci(results, type="bca")
+
+dev.off()
