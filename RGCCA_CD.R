5526a4317e0d352d307853ed3ebdb498aa79d3ec
llrs <lluis.revilla@gmail.com>
2018-02-01 13:01:37 +0100

    Add analysis for CD

---
+++ RGCCA_CD.R
@@ -0,0 +1,336 @@
+library("fgsea")
+
+# Load the helper file
+source("helper_functions.R")
+source("helper_RGCCA.R")
+load("filtered_data.RData", verbose = TRUE)
+library("ggforce")
+
+# Prepare meta for RGCCA
+# Filter only the IBD patients
+meta <- meta[meta$IBD == "CD", ]
+meta <- droplevels(meta)
+
+otus <- otus[, colnames(otus) %in% meta$Sample_Code]
+expr <- expr[, colnames(expr) %in% meta$Sample_Code]
+
+stopifnot(colnames(otus) == meta$Sample_Code)
+stopifnot(colnames(expr) == meta$Sample_Code)
+
+# Subset if all the rows are 0 and if sd is 0
+otus <- otus[apply(otus, 1, sd) != 0, ]
+# Filter expression
+expr <- norm_RNAseq(expr)
+
+# Select the features of metadata Time and Age_sample isn't the same?? perhaps removing them
+
+variables <- c(
+    "Patient_ID", "IBD", "Aftected_area",
+    "ANTITNF_responder", "TimeDiag", "Age",
+    "Clinical_activity", "Activity", "Gender",
+    "Endoscopic_Activity"
+)
+meta_R <- meta[, variables]
+
+meta_R[, variables] <- sapply(meta_R[, variables], function(x) {
+    if (is.character(x) | is.factor(x)) {
+        as.numeric(as.factor(x))
+    } else if (is.numeric(x)) {
+        x
+    }
+})
+meta_R[is.na(meta_R)] <- 0
+
+
+# Prepare input for the sgcca function
+A <- list(RNAseq = t(expr), "16S" = t(otus), "metadata" = meta_R)
+A <- sapply(A, function(x){
+    x[, apply(x, 2, sd) != 0]
+}, simplify = FALSE)
+
+saveRDS(A, file = "BCN_CD.RDS")
+
+# The design
+C <- matrix(
+    0, ncol = length(A), nrow = length(A),
+    dimnames = list(names(A), names(A))
+)
+C <- subSymm(C, "16S", "metadata", 1)
+C <- subSymm(C, "RNAseq", "metadata", 1)
+
+
+# We cannnot comput eht tau.estimate for A[[1]]
+# (shrinkage <- sapply(A, tau.estimate))
+shrinkage <- c(0.122747, 0, 1) # We guess a 0.1 for the RNAseq expression
+shrinkage[2] <- tau.estimate(A[[2]])
+(min_shrinkage <- sapply(A, function(x) {
+    1 / sqrt(ncol(x))
+}))
+# # Don't let the shrinkage go below the thershold allowed
+(shrinkage <- ifelse(shrinkage < min_shrinkage, min_shrinkage, shrinkage))
+# shrinkage <- rep(1, length(A))
+
+ncomp <- rep(2, length(A))
+
+sgcca.centroid <- sgcca(
+    A, C, c1 = shrinkage,
+    ncomp = ncomp,
+    scheme = "centroid",
+    scale = TRUE,
+    verbose = FALSE
+)
+names(sgcca.centroid$Y) <- names(A)
+names(sgcca.centroid$a) <- names(A)
+names(sgcca.centroid$astar) <- names(A)
+names(sgcca.centroid$AVE$AVE_X) <- names(A)
+sgcca.centroid$AVE$AVE_X <- simplify2array(sgcca.centroid$AVE$AVE_X)
+
+sgcca.factorial <- sgcca(
+    A, C, c1 = shrinkage,
+    ncomp = ncomp,
+    scheme = "factorial",
+    scale = TRUE,
+    verbose = FALSE
+)
+names(sgcca.factorial$Y) <- names(A)
+names(sgcca.factorial$a) <- names(A)
+names(sgcca.factorial$astar) <- names(A)
+names(sgcca.factorial$AVE$AVE_X) <- names(A)
+sgcca.factorial$AVE$AVE_X <- simplify2array(sgcca.factorial$AVE$AVE_X)
+
+sgcca.horst <- sgcca(
+    A, C, c1 = shrinkage,
+    ncomp = ncomp,
+    scheme = "horst",
+    scale = TRUE,
+    verbose = FALSE
+)
+names(sgcca.horst$Y) <- names(A)
+names(sgcca.horst$a) <- names(A)
+names(sgcca.horst$astar) <- names(A)
+names(sgcca.horst$AVE$AVE_X) <- names(A)
+sgcca.horst$AVE$AVE_X <- simplify2array(sgcca.factorial$AVE$AVE_X)
+
+# list(sgcca.centroid = sgcca.centroid, sgcca.horst = sgcca.horst,
+# sgcca.factorial = sgcca.factorial)
+save(sgcca.centroid, file = "CD.RData")
+
+samples <- data.frame(
+    "RNAseq" = sgcca.centroid$Y[["RNAseq"]][, 1],
+    "microbiota" = sgcca.centroid$Y[["16S"]][, 1]
+)
+
+
+## Grouping of the variables ####
+RNAseq1 <- samples$RNAseq
+RNAseq2 <- sgcca.centroid$Y[["RNAseq"]][, 2]
+microbiota2 <- sgcca.centroid$Y[["16S"]][, 2]
+microbiota1 <- samples$microbiota
+
+names(RNAseq1) <- rownames(samples)
+names(microbiota1) <- rownames(samples)
+names(RNAseq2) <- rownames(samples)
+names(microbiota2) <- rownames(samples)
+groups <- split(rownames(samples), as.factor(meta$ANTITNF_responder))
+# First dimension seems to capture well the
+fgsea(groups, RNAseq1, nperm = 1000)
+fgsea(groups, microbiota1, nperm = 1000)
+# Further dimensions
+fgsea(groups, RNAseq2, nperm = 1000)
+fgsea(groups, microbiota2, nperm = 1000)
+
+km <- kmeans(samples[, c("RNAseq", "microbiota")], 2, nstart = 2)
+plot(samples[, c("RNAseq", "microbiota")], col = km$cluster,
+     main = "kmeans clustering\nTrying to predict responders")
+
+## Plotting results ####
+# Colors for the plots
+samples <- cbind(samples, droplevels(meta))
+samples$Patient_ID <- as.factor(samples$Patient_ID)
+samples$Sample_Code <- as.character(samples$Sample_Code)
+
+pdf(paste0("Figures/", today, "_RGCCA_plots_IBD.pdf"))
+
+# Labels of the samples
+samples$Time <- as.factor(samples$Time)
+for (p in seq_along(levels(samples$Time))) {
+    a <- ggplot(samples, aes(RNAseq, microbiota)) +
+        geom_text(aes(label = Sample_Code)) +
+        geom_vline(xintercept = 0) +
+        geom_hline(yintercept = 0) +
+        ggtitle(paste0("Samples by time")) +
+        xlab("RNAseq (component 1)") +
+        ylab("16S (component 1)") +
+        guides(col = guide_legend(title = "Patient")) +
+        theme(plot.title = element_text(hjust = 0.5)) +
+        scale_color_manual(values = colors) +
+        facet_wrap_paginate(~Time, ncol = 1, nrow = 1, page = p)
+    print(a)
+}
+
+for (p in seq_along(levels(samples$Patient_ID))) {
+    a <- ggplot(samples, aes(RNAseq, microbiota)) +
+        geom_text(aes(color = Patient_ID, label = Time)) +
+        geom_vline(xintercept = 0) +
+        geom_hline(yintercept = 0) +
+        ggtitle(paste0("Samples by patient")) +
+        xlab("RNAseq (component 1)") +
+        ylab("16S (component 1)") +
+        guides(col = guide_legend(title = "Patient")) +
+        theme(plot.title = element_text(hjust = 0.5)) +
+        scale_color_manual(values = colors) +
+        facet_wrap_paginate(~Patient_ID, ncol = 1, nrow = 1, page = p)
+    print(a)
+}
+ggplot(samples, aes(RNAseq, microbiota)) +
+    geom_text(aes(color = Patient_ID, label = Time)) +
+    geom_vline(xintercept = 0) +
+    geom_hline(yintercept = 0) +
+    ggtitle("All samples at all times ") +
+    # xlab("RNAseq (component 1)") +
+    # ylab("16S (component 1)") +
+    guides(col = guide_legend(title = "Patient")) +
+    theme(plot.title = element_text(hjust = 0.5))
+
+
+ggplot(samples, aes(RNAseq, microbiota)) +
+    geom_text(aes(
+        color = ANTITNF_responder,
+        label = Sample_Code)) +
+    geom_vline(xintercept = 0) +
+    geom_hline(yintercept = 0) +
+    ggtitle("All samples at all times ") +
+    xlab("RNAseq (component 1)") +
+    ylab("16S (component 1)") +
+    guides(col = guide_legend(title = "Responders")) +
+    theme(plot.title = element_text(hjust = 0.5))
+
+
+ggplot(samples, aes(RNAseq, microbiota)) +
+    geom_text(aes(
+        color = Endoscopic_Activity,
+        label =  Sample_Code)) +
+    geom_vline(xintercept = 0) +
+    geom_hline(yintercept = 0) +
+    ggtitle("All samples at all times ") +
+    xlab("RNAseq (component 1)") +
+    ylab("16S (component 1)") +
+    guides(col = guide_legend(title = "Endoscopic Activity")) +
+    theme(plot.title = element_text(hjust = 0.5))
+
+ggplot(samples, aes(RNAseq, microbiota)) +
+    geom_text(aes(color = Time, label = Sample_Code)) +
+    geom_vline(xintercept = 0) +
+    geom_hline(yintercept = 0) +
+    ggtitle("All samples at all times ") +
+    xlab("RNAseq (component 1)") +
+    ylab("16S (component 1)") +
+    guides(col = guide_legend(title = "Time")) +
+    theme(plot.title = element_text(hjust = 0.5))
+
+variables <- data.frame(
+    Origin = rep(names(A), sapply(A, ncol)),
+    comp1 = unlist(sapply(
+        sgcca.centroid$a,
+        function(x) {
+            x[, 1]
+        }
+    )),
+    comp2 = unlist(sapply(
+        sgcca.centroid$a,
+        function(x) {
+            x[, 2]
+        }
+    ))
+)
+variables$var <- gsub("^.*\\.(OTU_.*)$", "\\1", rownames(variables))
+rownames(variables) <- gsub("^.*\\.(OTU_.*)$", "\\1", rownames(variables))
+variables$var <- gsub("^RNAseq\\.(ENSG.*)$", "\\1", rownames(variables))
+rownames(variables) <- gsub("^.*\\.(ENSG.*)$", "\\1", rownames(variables))
+rownames(variables) <- gsub("^metadata\\.(.*)$", "\\1", rownames(variables))
+variables$var <- gsub("^metadata\\.(.*)$", "\\1", rownames(variables))
+
+# Remove the variables that in both components are 0
+keepComp1RNAseq <- mean(abs(variables$comp1)[variables$Origin == "RNAseq"])
+keepComp1_16S <- mean(abs(variables$comp1)[variables$Origin == "16S"])
+keepComp1_metadata <- mean(abs(variables$comp1)[variables$Origin == "metadata"])
+
+keepComp2RNAseq <- mean(abs(variables$comp2)[variables$Origin == "RNAseq"])
+keepComp2_16S <- mean(abs(variables$comp2)[variables$Origin == "16S"])
+keepComp2_metadata <- mean(abs(variables$comp2)[variables$Origin == "metadata"])
+
+keepComp1 <- c(
+    abs(variables$comp1[variables$Origin == "RNAseq"]) > keepComp1RNAseq,
+    abs(variables$comp1[variables$Origin == "16S"]) > keepComp1_16S,
+    abs(variables$comp1[variables$Origin == "metadata"]) > keepComp1_metadata
+)
+keepComp2 <- c(
+    abs(variables$comp2[variables$Origin == "RNAseq"]) > keepComp2RNAseq,
+    abs(variables$comp2[variables$Origin == "16S"]) > keepComp2_16S,
+    abs(variables$comp2[variables$Origin == "metadata"]) > keepComp2_metadata
+)
+
+subVariables <- variables[keepComp1 & keepComp2, ]
+
+ggplot(subVariables, aes(comp1, comp2), color = Origin) +
+    geom_path(aes(x, y), data = circleFun(c(0, 0), 0.1, npoints = 100)) +
+    geom_path(aes(x, y), data = circleFun(c(0, 0), 0.2, npoints = 100)) +
+    geom_path(aes(x, y), data = circleFun(c(0, 0), 0.3, npoints = 100)) +
+    geom_path(aes(x, y), data = circleFun(c(0, 0), 0.4, npoints = 100)) +
+    geom_text(aes(color = Origin, label = var)) +
+    geom_vline(xintercept = 0) +
+    geom_hline(yintercept = 0) +
+    coord_cartesian() +
+    ggtitle(
+        "Variables important for the first two components",
+        subtitle = "Integrating stools and mucosa samples"
+    )
+
+rnaseq_i <- subVariables$var[subVariables$Origin == "RNAseq"]
+if (length(rnaseq_i) >= 2) {
+    pr <- prcomp(t(expr[rnaseq_i, ]), scale. = TRUE)
+    prS <- summary(pr)
+    ggplot(as.data.frame(pr$x),
+           aes(PC1, PC2,
+               color = as.factor(meta$ANTITNF_responder))) +
+        geom_point() +
+        xlab(paste("PC1", prS$importance[2, "PC1"] * 100, "%")) +
+        ylab(paste("PC2", prS$importance[2, "PC2"] * 100, "%")) +
+        ggtitle("RNAseq PCA from the important variables") +
+        guides(col = guide_legend(title = "Responders"))
+}
+
+micro_i <- subVariables$var[subVariables$Origin == "16S"]
+if (length(micro_i) >= 2) {
+    pr <- prcomp(t(otus[micro_i, ]), scale. = TRUE)
+    prS <- summary(pr)
+    ggplot(as.data.frame(pr$x),
+           aes(PC1, PC2,
+               color = as.factor(meta$ANTITNF_responder))) +
+        geom_point() +
+        xlab(paste("PC1", prS$importance[2, "PC1"] * 100, "%")) +
+        ylab(paste("PC2", prS$importance[2, "PC2"] * 100, "%")) +
+        ggtitle("16S PCA from the important variables") +
+        guides(col = guide_legend(title = "Responders"))
+}
+# Plot for the same component the variables of each block
+comp1 <- sapply(sgcca.centroid$a, function(x) {
+    x[, 1]
+})
+variables_weight(comp1)
+
+# Second component
+comp2 <- sapply(sgcca.centroid$a, function(x) {
+    x[, 2]
+})
+variables_weight(comp2)
+
+# Bootstrap of sgcca
+STAB <- boot_sgcca(A, C, shrinkage, 1000)
+
+save(STAB, file = "bootstrap_CD.RData")
+
+# Evaluate the boostrap effect and plot
+boot_evaluate(STAB)
+
+dev.off()
---
+++ br_CD.R
@@ -0,0 +1,165 @@
+# RGCCA on BCN CD
+
+load("filtered_data.RData")
+source("helper_functions.R")
+
+library("fgsea") # To test the distribution of the pathways
+library("reactome.db") # To get the pathways
+library("org.Hs.eg.db") # To get the names to entrez from the ensembl
+library("data.table")
+
+# Load previous data
+load("CD.RData")
+load("bootstrap_CD.RData")
+
+epithelium <- read.csv("epithelium.csv")
+epithelium <- epithelium$Epithelium
+
+pdf(paste0("Figures/", today, "_enrichments.pdf"))
+
+# RNAseq ####
+b <- STAB[["RNAseq"]]
+d <- sgcca.centroid$a[["RNAseq"]][, 1]
+
+# Remove duplicated if sgcca failed due to LAPACK subroutine
+b <- b[!is.na(b[, 1]), ]
+
+warning(sum(is.na(b[, 1])), " iterations failed.")
+
+# Test if the gene is significant by comparing to how much times is different
+# from 0 (because CCA tends to compensate itself)
+count <- apply(b, 2, function(x) sum(x != 0, na.rm = TRUE))
+freq <- count / nrow(b)
+
+library(ggplot2)
+ggplot(as.data.frame(cbind(freq, d))) +
+    geom_point(aes(d, freq)) +
+    xlab("Score") +
+    ylab("Frequency (!= 0)") +
+    ggtitle("RNAseq")
+
+# Select those genes that are significant
+significant <- names(d)[freq > 0.5]
+significant <- gsub("(.*)\\..*", "\\1", significant)
+
+loadings <- sgcca.centroid$a[["RNAseq"]]
+
+# Convert the ids to the entrezIds
+ensemblID <- rownames(loadings)
+ensemblID <- gsub("(.*)\\..*", "\\1", ensemblID)
+rownames(loadings) <- gsub("(.*)\\..*", "\\1", rownames(loadings))
+entrezID <- mapIds(
+    org.Hs.eg.db, keys = ensemblID, keytype = "ENSEMBL",
+    column = "ENTREZID"
+)
+comp1 <- loadings[, 1]
+names(comp1) <- entrezID
+
+
+epitheliumE <- mapIds(
+    org.Hs.eg.db, keys = as.character(epithelium),
+    keytype = "SYMBOL", column = "ENTREZID"
+)
+
+epitheliumE <- unlist(epitheliumE, use.names = TRUE)
+epitheliumE <- epitheliumE[!is.na(epitheliumE)]
+
+
+# Extract the information of the pathways
+genes2Pathways <- as.list(reactome.db::reactomeEXTID2PATHID)
+pathways <- unlist(genes2Pathways, use.names = FALSE)
+genes <- rep(names(genes2Pathways), lengths(genes2Pathways))
+paths2genes <- split(genes, pathways) # List of genes and the gene sets
+
+# Subset to only human pathways
+paths2genes <- paths2genes[grep("R-HSA-", names(paths2genes))]
+
+## Compute the hypergeometric/enrichment analysis ####
+library("ReactomePA")
+enrich <- enrichPathway(
+    gene = entrezID[significant], pvalueCutoff = 0.05,
+    readable = TRUE, universe = unique(entrezID)
+)
+if (nrow(enrich) == 0) {
+    warning("Enrichment didn't result in any pathway")
+}
+write.csv(as.data.frame(enrich), file = "RNAseq_enrichment.csv")
+
+# Store the entrezid
+entrezSig <- entrezID[significant]
+entrezSig <- entrezSig[!is.na(entrezSig)]
+paths2genes[["significant"]] <- entrezSig
+paths2genes[["Epithelium"]] <- epitheliumE
+
+## Compute the GSEA for the size effect ####
+gseaSizeEffect <- fgsea(paths2genes, comp1, nperm = length(comp1))
+
+# Get the name of the pathway
+namesPaths <- select(
+    reactome.db, keys = gseaSizeEffect$pathway,
+    keytype = "PATHID", columns = "PATHNAME"
+)
+# Remove the homo sapiens part
+namesPaths$PATHNAME <- gsub("Homo sapiens: (.*)", "\\1", namesPaths$PATHNAME)
+# Add a column
+gseaSizeEffect[, namesPaths := namesPaths$PATHNAME]
+# Order the dataframe by size effect
+data.table::setorder(gseaSizeEffect, -NES, padj, -size)
+if (sum(gseaSizeEffect$padj < 0.05) == 0) {
+    warning("GSEA didn't result in any pathway")
+}
+# Store the output
+fwrite(gseaSizeEffect[pval < 0.05, ], file = "gsea_RNAseq_pathways.csv")
+
+## 16S ####
+b <- STAB[["16S"]]
+d <- sgcca.centroid$a[["16S"]][, 1]
+
+# Test if the gene is significant by comparing to how much times is different
+# from 0 (because CCA tends to compensate itself)
+count <- apply(b, 2, function(x) sum(x != 0, na.rm = TRUE))
+freq <- count / nrow(b)
+
+ggplot(as.data.frame(cbind(freq, d))) +
+    geom_point(aes(d, freq)) +
+    xlab("Score") +
+    ylab("Frequency (!= 0)") +
+    ggtitle("16S")
+
+otus <- names(d)[freq > 0.5]
+
+## Split the taxonomy
+Taxon2Class <- as.list(as.data.frame(tax))
+
+grouping <- split(Taxon2Class$Genus, Taxon2Class$Genus)
+grouping <- sapply(grouping, names)
+
+term2gene <- data.frame(
+    "Gene" = tax[, "Genus"],
+    "Term" = rownames(tax)
+)
+term2name <- data.frame(
+    "Name" = tax[, "Genus"],
+    "Term" = rownames(tax)
+)
+
+library("clusterProfiler")
+enrich <- as.data.frame(enricher(
+    gene = otus, universe = rownames(tax),
+    minGSSize = 1, TERM2GENE = term2gene,
+    TERM2NAME = term2name
+))
+if (nrow(enrich) == 0) {
+    warning("Enrichment didn't result in any pathway")
+}
+write.csv(enrich, file = "Otus_genus_enrichment.csv")
+
+# GSEA
+comp1 <- sgcca.centroid$a[["16S"]][, 1]
+
+gseaSizeEffect <- fgsea(grouping, comp1, nperm = 20000)
+data.table::setorder(gseaSizeEffect, -NES, padj, -size)
+if (sum(gseaSizeEffect$padj < 0.05) == 0) {
+    warning("GSEA didn't result in any pathway")
+}
+fwrite(gseaSizeEffect[pval < 0.05], file = "gsea_otus_genus.csv")
